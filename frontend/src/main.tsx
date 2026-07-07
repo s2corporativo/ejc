03eb849cfd4351d948d088b2e051e9e8b393ddd2
S2 CORPORATIVO <adm@vetmg.com.br>
2026-07-07 11:14:27 -0300

    feat(workspace): carrega visual executivo

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { initTheme } from "./stores/theme";
 import "./index.css";
 import "./styles/site-system.css";
 import "./styles/clientes-casos-saas.css";
+import "./styles/workspace-executive.css";
 
 initTheme();
 
